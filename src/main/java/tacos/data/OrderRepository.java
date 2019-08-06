3170cb0858430d3116ae979a0c74a2bc9d781cf4
Pavlo Velykyi <pavlo.velykyi@globallogic.com>
2019-08-06 11:56:17 +0300

    added config properties

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,14 +1,16 @@
 package tacos.data;
 
-import org.aspectj.weaver.ast.Or;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
+import tacos.User;
 
 import java.util.List;
 
 public interface OrderRepository
         extends CrudRepository<Order, Long> {
     List<Order> findByZip(String deliveryZip);
+
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 
 }
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,7 +1,12 @@
 package tacos.web;
 
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +14,8 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-import reactor.netty.udp.UdpServer;
 import tacos.Order;
 import tacos.User;
 import tacos.data.OrderRepository;
@@ -20,12 +25,14 @@ import javax.validation.Valid;
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
 
     private OrderRepository orderRepo;
+    private OrderProps orderProps;
 
     @Autowired
-    public OrderController(OrderRepository orderRepo){
+    public OrderController(OrderRepository orderRepo) {
         this.orderRepo = orderRepo;
     }
 
@@ -38,9 +45,9 @@ public class OrderController {
     public String processOrder(
             @Valid Order order, Errors errors,
             SessionStatus sessionStatus,
-            @AuthenticationPrincipal User user){
+            @AuthenticationPrincipal User user) {
 
-        if (errors.hasErrors()){
+        if (errors.hasErrors()) {
             return "orderForm";
         }
         order.setUser(user);
@@ -51,4 +58,15 @@ public class OrderController {
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
+
+    public String ordersForUser(
+            @AuthenticationPrincipal User user, Model model) {
+
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+        model.addAttribute("orders",
+                orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+
+        return "orderList";
+    }
+
 }
